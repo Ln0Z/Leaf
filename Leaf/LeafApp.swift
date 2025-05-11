bb462bb2b44158df0142c1a55fe0097834b32be6
AspiringDSRianna <rianna.libdy@student.uts.edu.au>
2025-05-11 14:11:29 +1000

    Initial Commit

---
+++ Leaf/LeafApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafApp.swift
+//  Leaf
+//
+//  Created by Rianna Libdy on 11/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct LeafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
